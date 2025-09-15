a310cb4d9eea8d9f5f6dad52a07f46da8ef8244e
Derrick <pshubomi@gmail.com>
2025-09-15 16:43:51 +0100

    :

--- src/modules/form/form.controller.ts
+++ src/modules/form/form.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { FormService } from './form.service';
 import { CreateFormDto } from './dto/create-form.dto';
+import { Response } from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Controller('form')
 export class FormController {
   constructor(private readonly formService: FormService) {}
+  private filePath = path.join(process.cwd(), 'src/data/forms.xlsx');
 
   @Post(':sheetName')
   async saveForm(
@@ -12,5 +16,14 @@ export class FormController {
     @Param('sheetName') sheetName: string,
   ) {
     return this.formService.saveFormData(createFormDto, sheetName);
+  }
+
+  @Get('download')
+  async downloadFile(@Res() res: Response) {
+    if (fs.existsSync(this.filePath)) {
+      res.download(this.filePath, 'forms.xlsx'); // force download
+    } else {
+      res.status(404).send('File not found');
+    }
   }
 }
